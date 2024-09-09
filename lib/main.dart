06ab12ca6a61a8ae19c541fad1300cb8998be914
Amr Assem <amr.assem@onmail.com>
2024-09-09 13:09:20 +0300

    feat: configure and setup Firebase for project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:eventure/screens/onboarding_screens/onboarding_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
