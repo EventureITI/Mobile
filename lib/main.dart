cf253486e988554d402a0625cdb69642a6fc39ed
Amr Assem <amr.assem@onmail.com>
2024-09-10 02:04:54 +0300

    chore: configure Stripe for the project

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:eventure/screens/onboarding_screens/onboarding_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
 
 void main() async {
@@ -13,6 +14,7 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  Stripe.publishableKey = dotenv.env['STRIPE_PK']!;
   // print(DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
