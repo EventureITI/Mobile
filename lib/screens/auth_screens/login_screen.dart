d07d159d6a7ca00408d506ec9fb770021a8c87ce
kerollos <kerollosromany487@gmail.com>
2024-09-11 23:43:18 +0300

    feat: add validation

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -1,9 +1,15 @@
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:eventure/screens/auth_screens/firebase_auth_impl/firebase_services.dart';
+import 'package:eventure/screens/auth_screens/firebase_auth_impl/user_controller.dart';
 import 'package:eventure/screens/auth_screens/signup_screen.dart';
+import 'package:eventure/utils/validators.dart';
 import 'package:eventure/screens/home_screens/home_screen.dart';
 import 'package:eventure/utils/text_colors.dart';
 import 'package:eventure/widgets/custom_input_field.dart';
 import 'package:eventure/widgets/custom_text.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -14,12 +20,28 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool _obscureText = true;
+  final _loginFormKey = GlobalKey<FormState>();
+
+  List<String> loginForm=[];
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  UserController userController= Get.put(UserController());
 
   void togglePwVisiblity() {
     setState(() {
       _obscureText = !_obscureText;
       // print(_obscureText);
     });
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controllers when the widget is disposed
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
   }
 
   @override
@@ -41,66 +63,135 @@ class _LoginScreenState extends State<LoginScreen> {
                   size: 32,
                   ftWeight: FontWeight.w600,
                 ),
-                Column(
-                  children: <Widget>[
-                    CustomInputfield(
-                      hint: "example@mail.com",
-                      label: "Email",
-                      scure: false,
-                      inpType: TextInputType.emailAddress,
-                    ),
-                    SizedBox(height: 24),
-                    Column(
-                      children: <Widget>[
-                        CustomInputfield(
-                          hint: "Enter your password",
-                          label: "Password",
-                          scure: _obscureText,
-                          suffixIcn: IconButton(
-                            icon: Icon(_obscureText
-                                ? Icons.visibility_off
-                                : Icons.visibility),
-                            onPressed: () {
-                              togglePwVisiblity();
-                            },
-                            color: iconColor,
-                          ),
-                        ),
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          alignment: AlignmentDirectional.centerEnd,
-                          child: TextButton(
-                            onPressed: () {},
-                            child: CustomText(text: "Forgot password?"),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(height: 24),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: 48,
-                      child: ElevatedButton(
-                        onPressed: () {
-                          Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute<void>(
-                                builder: (BuildContext context) =>
-                                    const HomeScreen(),
-                              ));
+                Form(
+                  key: _loginFormKey,
+                  child: Column(
+                    children: <Widget>[
+                      CustomInputfield(
+                        hint: "example@mail.com",
+                        label: "Email",
+                        scure: false,
+                        inpType: TextInputType.emailAddress,
+                        controller: _emailController,
+                        validator: (value) {
+                          if(value == null || value.isEmpty ){
+                            return "Email is required";
+                          }
+                          else if(!emailRegExp.hasMatch(value)){
+                            return "Please, Enter a valid Email";
+                          }
+                          else{
+                            return null;
+                          }
                         },
-                        style: ElevatedButton.styleFrom(
-                            backgroundColor: btnColor,
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15))),
-                        child: CustomText(
-                          text: "Sign In",
-                          color: Colors.white,
-                          ftWeight: FontWeight.w600,
+                      ),
+                      SizedBox(height: 24),
+                      Column(
+                        children: <Widget>[
+                          CustomInputfield(
+                            hint: "Enter your password",
+                            label: "Password",
+                            scure: _obscureText,
+                            controller: _passwordController,
+                            suffixIcn: IconButton(
+                              icon: Icon(_obscureText
+                                  ? Icons.visibility_off
+                                  : Icons.visibility),
+                              onPressed: () {
+                                togglePwVisiblity();
+                              },
+                              color: iconColor,
+                            ),
+                            validator: (value) {
+                              if(value == null || value.isEmpty ){
+                                return "Password is required";
+                              }
+                              else if(value.length < 6){
+                                return "Password must be at least 6 characters";
+                              }
+                              else{
+                                return null;
+                              }
+                            },
+                          ),
+                          Container(
+                            width: MediaQuery.of(context).size.width,
+                            alignment: AlignmentDirectional.centerEnd,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: CustomText(text: "Forgot password?"),
+                            ),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 24),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        height: 48,
+                        child: ElevatedButton(
+                          onPressed: () async{
+                            if(_loginFormKey.currentState!.validate()){
+
+                              loginForm = [
+                                _emailController.text,
+                                _passwordController.text,
+                              ];
+
+                              print(loginForm);
+                              User? user = await _auth.signInWithEmailAndPassword(_emailController.text, _passwordController.text);
+                              if(user !=null){
+                                print("USER LOGGED IN");
+                                print(user);
+                                Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute<void>(
+                                    builder: (BuildContext context) =>
+                                        HomeScreen(),
+                                  ),
+                                );
+                                // Get.off(()=>HomeScreen());
+                                
+                                userController.loggedIn();
+
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    backgroundColor: Colors.transparent,
+                                    content: AwesomeSnackbarContent(
+                                      color: btnColor,
+                                      title: "Signed In", 
+                                      message: "Welcom Back, Have Fun!!", 
+                                      contentType: ContentType.success
+                                    ),
+                                  ),
+                                );
+                              }else{
+                                print("error occured");
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  SnackBar(
+                                    backgroundColor: Colors.transparent,
+                                    content: AwesomeSnackbarContent(
+                                      title: "Couldn't Sign in", 
+                                      message: "Email and Password are Invalid", 
+                                      contentType: ContentType.failure
+                                    ),
+                                  ),
+                                );
+                              }
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: btnColor,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15))),
+                          child: CustomText(
+                            text: "Sign In",
+                            color: Colors.white,
+                            ftWeight: FontWeight.w600,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height / 5),
                 Row(
--- lib/widgets/custom_input_field.dart
+++ lib/widgets/custom_input_field.dart
@@ -1,19 +1,31 @@
-import 'package:eventure/utils/text_colors.dart';
+// import 'dart:async';
+// import 'dart:nativewrappers/_internal/vm/lib/core_patch.dart';
+
 import 'package:flutter/material.dart';
+import 'package:eventure/utils/text_colors.dart';
+import 'package:flutter/widgets.dart';
 
 class CustomInputfield extends StatelessWidget {
+
+  FormFieldValidator<String>? validator;
+  TextEditingController? controller;
   TextInputType? inpType;
   String? label;
   bool? scure;
   Widget? suffixIcn;
   String? hint;
   CustomInputfield(
-      {super.key,
+    {
+      super.key,
       this.hint,
       this.label,
       this.scure,
       this.suffixIcn,
-      this.inpType});
+      this.inpType,
+      this.validator,
+      this.controller
+    }
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +44,9 @@ class CustomInputfield extends StatelessWidget {
           height: 8,
         ),
         TextFormField(
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          controller: controller,
+          validator: validator,
           keyboardType: inpType,
           obscureText: scure!,
           style: TextStyle(color: Colors.white),
@@ -40,6 +55,7 @@ class CustomInputfield extends StatelessWidget {
               fillColor: inpBg,
               hintText: hint,
               hintStyle: TextStyle(color: hintColor, fontSize: 14),
+              errorStyle: TextStyle(fontSize: 10),
               suffixIcon: suffixIcn,
               border:
                   OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
