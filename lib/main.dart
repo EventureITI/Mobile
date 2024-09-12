4e32b83896acb97dc7505904417fbdeca0bad76e
ahmedmansour200 <ahmedmansour10122000@gmail.com>
2024-09-13 01:22:03 +0300

    feat: add the no internet screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eventure/firebase_options.dart';
 import 'package:eventure/screens/home_screens/home_screen.dart';
+import 'package:eventure/screens/no_internet_screen.dart';
 import 'package:eventure/screens/onboarding_screens/onboarding_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:get/get.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
+import 'dart:io';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,10 +30,44 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Eventure',
-        theme: ThemeData.dark(),
-        home: HomeScreen());
+    return FutureBuilder<bool>(
+      future: chuckCoated(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Show a loading indicator while checking
+          return MaterialApp(
+              home: Scaffold(body: Center(child: CircularProgressIndicator())));
+        } else if (snapshot.hasData && snapshot.data == true) {
+          // If connected, show the HomeScreen
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Eventure',
+            theme: ThemeData.dark(),
+            home: HomeScreen(),
+          );
+        } else {
+          // If not connected, show NoInternetScreen
+          return GetMaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Eventure',
+            theme: ThemeData.dark(),
+            home: NoInternetScreen(),
+          );
+        }
+      },
+    );
+  }
+}
+
+Future<bool> chuckCoated() async {
+  try {
+    final result = await InternetAddress.lookup('google.com');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  } on SocketException catch (_) {
+    return false;
   }
 }
---
+++ lib/screens/no_internet_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class NoInternetScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.wifi_off, size: 100, color: Colors.red),
+            SizedBox(height: 20),
+            Text('No Internet Connection',
+                style: TextStyle(fontSize: 24, color: Colors.red)),
+          ],
+        ),
+      ),
+    );
+  }
+}
